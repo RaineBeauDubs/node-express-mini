a27006aba1dbb71ce84a498ccec3612c769f3bc2
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-18 15:36:37 -0500

    Users rendering.

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ server.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+server.get('/api/users', (req, res) => {
+  db
+    .find()
+    .then(users => {
+      res.status(201).json(users)
+    })
+    .catch(({ code, message }) => {
+      res.status(code).json({ success: false, message })
+    })
+})
+
 server.listen(4000, () => {
   console.log(`\n*** Server Running on http://localhost:4000 ***\n`)
 });
