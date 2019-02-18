41dd7f00460a182d77eb8518f2cb39d32fbd80fd
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-18 15:28:57 -0500

    Server is set up/listening/tested.

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 
 const express = require('express');
 
+const db = require('./data/db');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+server.listen(4000, () => {
+  console.log(`\n*** Server Running on http://localhost:4000 ***\n`)
+});
