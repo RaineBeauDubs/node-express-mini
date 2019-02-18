07b2f8f5309fb29137094e32541ef11f1f1f717f
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-18 15:58:54 -0500

    I now have the ability to post new hobbits.

--- index.js
+++ index.js
@@ -16,10 +16,41 @@ server.get('/api/users', (req, res) => {
   db
     .find()
     .then(users => {
-      res.status(201).json(users)
+      res
+        .status(201)
+        .json({ success: true, users})
     })
-    .catch(({ code, message }) => {
-      res.status(code).json({ success: false, message })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          success: false,
+          error: 'There was an error while saving the user to the database'
+        })
+    })
+})
+
+server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
+  const newUser = req.body;
+  if (!name || !bio) {
+    return res
+      .status(400)
+      .json({ errorMessage: 'Please provide name and bio for the user.' })
+  }
+  db
+    .insert(newUser)
+    .then(user => {
+      res.status(201).json({ success: true, user })
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          success: false,
+          error: 'There was an error while saving the user to the database'
+        })
+      res.end();
     })
 })
 
